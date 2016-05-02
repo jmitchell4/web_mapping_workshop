7823eb90b4c5fd29c2a7b5bdeec5eb87a099d756
jmitchell4 <jmitchell@capsci.com>
2016-05-02 17:19:17 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -147,8 +147,8 @@ function getDirections(frm, to) {
       }, 
       properties: {
         "stroke": "#ffff00", 
-        "stroke-opacity": "0.5", 
-        "stroke-width": "8"
+        "stroke-opacity": 0.5, 
+        "stroke-width": 8
       }
     };
     console.error(json);
